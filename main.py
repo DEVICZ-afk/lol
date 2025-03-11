02b4466abf33140ef9a5f56ce302e26e036527d2
unknown <david.jakes@infis.cz>
2025-03-11 09:30:48 +0100

    first

---
+++ main.py
@@ -0,0 +1,15 @@
+#Jakeš
+cislo = int(input("Zadejte vaše oblíbené číslo od 1 do 10: "))
+#input načte data od uživatele
+if cislo == 7:
+    print("máme stejné oblíbené číslo")
+#když je cislo stejné jako uživatelovo tak se vypíše tato věta
+elif cislo == 10 or cislo == 5:
+    print("To jsou oblíbená čísla mého kamaráda")
+#když je cislo stejné jako uživatelovo tak se vypíše tato věta
+else:
+    print("krásné číslo")
+#když je cislo jiné tak se vypíše tato věta
+#git příkaz 1 = git add *
+#git příkaz 2 = git commit -m "text"
+#git příkaz 3 = git push
