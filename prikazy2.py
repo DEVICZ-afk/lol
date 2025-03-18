4081cb2ac8f4a2729e8ba735d8359b076a05e000
unknown <david.jakes@infis.cz>
2025-03-18 09:27:53 +0100

    prikazy2

--- prikazy2.py
+++ prikazy2.py
@@ -2,14 +2,19 @@ rok = 2025
 text = "Veselé Velikonoce"
 cislo = 18361936
 
+#vypsání pomocí print
 print(f"Ahoj, {text} {rok}.")
 
+#čísla
 print(f"{cislo}")
 
+#prvních 6 čísel
 print(f"{text:.6}")
 
+#desetinná čísla
 print(f"{cislo:.3f}")
 
+#převody jednotak
 hodnota = 155
 print(f"{hodnota:b}")
 print(f"{hodnota:x}")
